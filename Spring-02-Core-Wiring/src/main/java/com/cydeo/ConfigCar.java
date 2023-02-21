8d90c145cdfbd5f7d967e1225c1ab3124964d64a
zehrairkicatal <zehraie197@gmail.com>
2023-02-21 20:00:56 +0000

    fixing error message

--- Spring-02-Core-Wiring/src/main/java/com/cydeo/ConfigCar.java
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/ConfigCar.java
@@ -2,30 +2,38 @@ package com.cydeo;
 
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@CacheConfig
+@Configuration
 public class ConfigCar {
 @Bean
     Car car(){
         Car c = new Car();
         c.setMake("Honda");
         return c;
+    }      // has relationship icin bizim wiring e ihtiyacimiz var cagirabilmek icin
+
+    @Bean
+    Car car2(){
+    Car z =new Car();
+    z.setMake("BMW");
+    return z;
     }
     //Direct wiring
-//    @Bean
-//    Person person(){
-//        Person p = new Person();
-//        p.setName("Mike");
-//        p.setCar(car());
-//        return p;
-//    }
-
-    //Autowiring
     @Bean
-    Person person(Car car){
-    Person p =new Person();
-    p.setName("Mike");
-    p.setCar(car);
-    return p;
+    Person person(){
+        Person p = new Person();
+        p.setName("Mike");
+        p.setCar(car());
+        return p;
     }
+
+    //Autowiring    ********** same result alirsin , autowiring is better
+//    @Bean
+//    Person person(Car car){
+//    Person p =new Person();
+//    p.setName("Mike");
+//    p.setCar(car);  //
+//    return p;
+//    }
 }
