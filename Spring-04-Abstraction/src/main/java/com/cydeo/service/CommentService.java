7974ee5aa5d10f40534f609dd555533245ab4041
zehrairkicatal <zehraie197@gmail.com>
2023-02-22 20:46:58 +0000

    publish comment1

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    private void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
