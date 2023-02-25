dbde295cc1996386f536be46aa753df73e61f63b
zehrairkicatal <zehraie197@gmail.com>
2023-02-25 17:01:47 +0000

    task_3

--- spring-review-01-IoC/src/main/java/stereotype_annotations/model/DataStructure.java
+++ spring-review-01-IoC/src/main/java/stereotype_annotations/model/DataStructure.java
@@ -1,10 +1,26 @@
 package stereotype_annotations.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component
+//@AllArgsConstructor
+@Data
 public class DataStructure {
+//    @Autowired
+//ExtraHours extraHours;   bu sekilde sadece constructor olmadan field injection, asagidaki ise constructor ile
+  @NonNull
+    ExtraHours extraHours;
+  //  @Autowired   // NORMALDE ANOTATION KOYMAYA GEREK YOK
+//    public DataStructure(ExtraHours extraHours) {
+//        this.extraHours = extraHours;
+//    }
+
     public void getTotalHours(){
-        System.out.println("Total hours : "+35);
+        System.out.println("Total hours : "+(35+ extraHours.getHours()));
     }
 }
---
+++ spring-review-01-IoC/src/main/java/stereotype_annotations/model/ExtraHours.java
@@ -0,0 +1,11 @@
+package stereotype_annotations.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExtraHours {
+
+    public Integer getHours(){
+        return 10;
+    }
+}
