ebd151485e93729732d159c25438cacd8dd0e02c
zehrairkicatal <zehraie197@gmail.com>
2023-02-26 15:55:07 +0000

    Spring_MVC_model_1

---
+++ Spring-08-MVC-Controller/src/main/java/com/cydeo/Spring08MvcControllerApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08MvcControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08MvcControllerApplication.class, args);
+    }
+
+}
---
+++ Spring-08-MVC-Controller/src/test/java/com/cydeo/spring08mvccontroller/Spring08MvcControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring08mvccontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring08MvcControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
