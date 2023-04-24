b8b9c63051b30fad37adb163545e6185803d4af5
zehrairkicatal <zehraie197@gmail.com>
2023-04-24 09:40:47 +0100

    manyToMany

---
+++ Spring-12-ORM-OneToMany-MantyToMany/src/main/java/com/cydeo/model/Card.java
@@ -0,0 +1,20 @@
+package com.cydeo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="carts")
+@Data
+@NoArgsConstructor
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ @ManyToMany
+    private List<Item> itemList;
+}
---
+++ Spring-12-ORM-OneToMany-MantyToMany/src/main/java/com/cydeo/model/Item.java
@@ -0,0 +1,25 @@
+package com.cydeo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="items")
+@Data
+@NoArgsConstructor
+public class Item { // there is manyToMany between cart and item, you need helper table
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToMany(mappedBy = "itemList")// card can
+    private List<Card> card;// use Set in some occasition
+    // items_card will be created and we call this join table
+    // who is ownership, you can pick it up yourself
+}
