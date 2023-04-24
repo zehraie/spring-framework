8c4e0da1d60dd33f95843db7207bcf884740edc4
zehrairkicatal <zehraie197@gmail.com>
2023-04-24 17:25:56 +0100

    Cinema Lab

--- Spring-12-ORM-OneToMany-MantyToMany/src/main/java/com/cydeo/model/Card.java
+++ Spring-12-ORM-OneToMany-MantyToMany/src/main/java/com/cydeo/model/Card.java
@@ -16,5 +16,9 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
  @ManyToMany
+ // you mark it is joinTable
+ @JoinTable(name="card_item_relationship"
+ ,joinColumns = @JoinColumn(name="c_id")
+ ,inverseJoinColumns = @JoinColumn(name="i_id"))
     private List<Item> itemList; // card is ownership
 }
